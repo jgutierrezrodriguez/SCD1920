a2817831fdfc4765199f77dd3f90e0671f5f2450
jgutierrezrodriguez <jgr00053@red.ujaen.es>
2020-02-20 13:59:52 +0100

    Finalizado ejercicio 2

--- P/S3/EjerciciosAniosAnteriores/1819Grupo2/src/pkg1819grupo2/Main.java
+++ P/S3/EjerciciosAniosAnteriores/1819Grupo2/src/pkg1819grupo2/Main.java
@@ -6,6 +6,8 @@
 package pkg1819grupo2;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
@@ -23,8 +25,63 @@ public class Main {
     public static final int PEDIDO_ORDENADORES = 10;
     public static final int NUM_TAREAS_FINALIZADAS = 1;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
+        Thread[] hilosComponentes;
+        Thread[] hilosOrdenadores;
+        int[] componentesCreados;
+        ReentrantLock exm;
+        CountDownLatch finTrabajo;
+        
+        System.out.println("Ha iniciado la ejecución el Hilo(PRINCIPAL)");
+        
+        // Creamos los recursos compartidos con las tareas
+        componentesCreados = new int[TIPOS_COMPONENTES.length];
+        for( int i = 0; i < componentesCreados.length; i++)
+            componentesCreados[i] = 0;
+        exm = new ReentrantLock();
+        finTrabajo = new CountDownLatch(NUM_TAREAS_FINALIZADAS);
+        
+        // Creamos las tareas para la creación de componentes 
+        hilosComponentes = new Thread[NUM_TAREAS_COMPONENTES];
+        for( int i = 0; i < NUM_TAREAS_COMPONENTES; i++ ){
+            TareaCrearComponente tareaComponente = new TareaCrearComponente(componentesCreados, exm);
+            hilosComponentes[i] = new Thread(tareaComponente, "Componente-" + i);
+            hilosComponentes[i].start();
+        } 
+        
+        // Creamos las tareas para la creación de ordenadores
+        hilosOrdenadores = new Thread[NUM_TAREAS_ORDENADOR];
+        for( int i = 0; i < NUM_TAREAS_ORDENADOR; i++ ) {
+            TareaCrearOrdenador tareaOrdenador = new TareaCrearOrdenador(componentesCreados, exm, finTrabajo);
+            hilosOrdenadores[i] = new Thread(tareaOrdenador, "Ordenador-" + i);
+            hilosOrdenadores[i].start();
+        }
+        
+        // Sincronización hasta que una tarea complete los ordenadores solicitados
+        System.out.println("Hilo(PRINCIPAL) espera hasta que una tarea complete el pedido de ordenadores");
+        finTrabajo.await();
+        
+        // Se cancelan el resto de las tareas
+        System.out.println("Hilo(PRINCIPAL) cancela el resto de las tareas");
+        for( Thread hilo : hilosComponentes )
+            hilo.interrupt();
+        for( Thread hilo : hilosOrdenadores )
+            hilo.interrupt();
+        
+        // Espera a que finalicen todas las tareas
+        System.out.println("Hilo(PRINCIPAL) espera a la finalización de las tareas");
+        for( Thread hilo : hilosComponentes )
+            hilo.join();
+        for( Thread hilo : hilosOrdenadores )
+            hilo.join();
+        
+        System.out.println("Hilo(PRINCIPAL) Los componentes creados que aún quedan: ");
+        for( ComponenteOrdenador componente : TIPOS_COMPONENTES ) {
+            System.out.println(componente + ": " + componentesCreados[componente.ordinal()]);
+        }
+        
+        System.out.println("Ha finalizado la ejecución el Hilo(PRINCIPAL)");
     }
     
 }
--- P/S3/EjerciciosAniosAnteriores/1819Grupo2/src/pkg1819grupo2/TareaCrearOrdenador.java
+++ P/S3/EjerciciosAniosAnteriores/1819Grupo2/src/pkg1819grupo2/TareaCrearOrdenador.java
@@ -6,8 +6,12 @@
 package pkg1819grupo2;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
+import pkg1819grupo2.Main.ComponenteOrdenador;
 import static pkg1819grupo2.Main.PEDIDO_ORDENADORES;
+import static pkg1819grupo2.Main.TIPOS_COMPONENTES;
+import static pkg1819grupo2.Main.generador;
 
 /**
  *
@@ -19,6 +23,9 @@ public class TareaCrearOrdenador implements Runnable{
     private ReentrantLock mutex;
     private CountDownLatch finTarea;
     private int ordenadoresCompletados;
+    
+    private static final int TIEMPO_MINIMO = 2;
+    private static final int INTERVALO_TIEMPO = 2;
 
     public TareaCrearOrdenador(int[] componentesCreados, ReentrantLock mutex, CountDownLatch finTarea) {
         this.componentesCreados = componentesCreados;
@@ -29,13 +36,62 @@ public class TareaCrearOrdenador implements Runnable{
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            System.out.println("El HILO(" + Thread.currentThread().getName() + ") Comienza la ejecución");
+            
+            crearOrdenador();
+            
+            // Indicamos al hilo principal que hemos terminado el trabajo
+            finTarea.countDown();
+            
+            System.out.println("El HILO(" + Thread.currentThread().getName() + ") Finaliza la ejecución");
+        } catch (InterruptedException ex) {
+            System.out.println("El HILO(" + Thread.currentThread().getName() + ") Ha sido cancelado");
+        }
     }
     
-    private void crearOrdenador(){
+    private void crearOrdenador() throws InterruptedException{
         while(ordenadoresCompletados < PEDIDO_ORDENADORES){
+            boolean disponible = false;
+            while ( !disponible ) {
+                // Comprobamos si hay interrupción
+                if ( Thread.currentThread().isInterrupted())
+                    throw new InterruptedException();
+                
+                // Garantizamos el acceso en exclusion mutua
+                mutex.lock();
+                try {
+                    disponible = hayComponentes();
+                    
+                    if ( disponible )
+                        // Decontamos los compoentes para el ordenador
+                        for( ComponenteOrdenador componente : TIPOS_COMPONENTES )
+                            componentesCreados[componente.ordinal()]--;
+                    
+                } finally {
+                    mutex.unlock();
+                }
+            }
             
+            int tiempo = TIEMPO_MINIMO + generador.nextInt(INTERVALO_TIEMPO);
+            TimeUnit.SECONDS.sleep(tiempo);
+            ordenadoresCompletados++;
+            System.out.println("El HILO(" + Thread.currentThread().getName() + 
+                        ") ha completado: " + ordenadoresCompletados + " ordenadores");
         }
+    }
+    
+    private boolean hayComponentes() throws InterruptedException {
+        int indice = 0;
+        boolean disponible = true;
+        
+        while ( (indice < componentesCreados.length) && disponible ) {
+            if ( componentesCreados[indice] == 0 ) 
+                disponible = false;
+            indice++;
+        }
+        
+        return disponible;
     }
     
     
